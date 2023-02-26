7f8c0f9f7874851be8f406f7e2824a65e53839e4
Alejandro Rojas Castro <81925776+akiiop@users.noreply.github.com>
2023-02-26 17:08:06 -0300

    ExpVal class initial commit

--- expectation_value/expectationvalue.py
+++ expectation_value/expectationvalue.py
@@ -1,1 +1,16 @@
 # This file will call all the other files which will have to be in this folder
+class ExpVal():
+    def __init__(self,
+                 n_shots,
+                 bodies,
+                 quantum_instace
+                ):
+        ExpVal.interferences = None
+    
+    def get_interferences( self, state, r, shots_r ):
+        
+        
+        self.interferences = resultado
+        return resultado
+        
+    def exp_val( self,  ):
